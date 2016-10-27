145427f656265da811e0a8c678f9982f97edee5f
Lina Shadrach and Maxwell Cady <lina@>
2016-10-27 15:59:10 -0700

    add styling to front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,44 @@
-var input = "hello world";
-var inputLetters = input.replace(/[^a-zA-Z]/g, "");
-var inputArray = inputLetters.split("");
-var inputLength = inputArray.length;
-var rows = Math.ceil(Math.sqrt(inputLength));
-var cols = Math.ceil(inputLength / rows);
+var cryptosquare = function(input) {
+  var inputLetters = input.replace(/[^a-zA-Z]/g, "");
+  var inputArray = inputLetters.split("");
+  var inputLength = inputArray.length;
+  var rows = Math.ceil(Math.sqrt(inputLength));
+  var cols = Math.ceil(inputLength / rows);
 
-var grid = [];
+  var grid = [];
 
-var row = [];
-for (var i = 0; i < rows; i++) {
-  row = [];
-  for (var j = 0; j < cols; j++) {
-    row.push(inputArray[j]);
+  var row = [];
+  for (var i = 0; i < rows; i++) {
+    row = [];
+    for (var j = 0; j < cols; j++) {
+      row.push(inputArray[j]);
+    }
+    inputArray.splice(0, cols);
+
+    grid.push(row);
   }
-  inputArray.splice(0, cols);
 
-  grid.push(row);
-}
-
-var letters = [];
-for (var i = 0; i < cols; i++) {
-  for (var j = 0; j < rows; j++) {
-    letters.push(grid[j][i]);
+  var letters = [];
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      letters.push(grid[j][i]);
+    }
   }
-}
 
-var lettersString = letters.join("");
+  var lettersString = letters.join("");
+  var outputArray = lettersString.match(/.{1,5}/g);
+  var output = outputArray.join(" ");
+  console.log(output);
+  return output;
+};
+
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input").val();
+    console.log(userInput);
+    var result = cryptosquare(userInput);
+    console.log(result);
+    $("#output").text(result);
+  })
+})
