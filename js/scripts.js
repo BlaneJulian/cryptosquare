bf5a779be60d1dc2b88528bc7cf7cfee0af73e5b
Lina Shadrach and Maxwell Cady <maxwell@>
2016-10-27 15:26:35 -0700

    take input and encodes it into a string based on grid

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,27 @@ var input = "hello world";
 var inputLetters = input.replace(/[^a-zA-Z]/g, "");
 var inputArray = inputLetters.split("");
 var inputLength = inputArray.length;
-console.log(inputLength);
+var rows = Math.ceil(Math.sqrt(inputLength));
+var cols = Math.ceil(inputLength / rows);
+
+var grid = [];
+
+var row = [];
+for (var i = 0; i < rows; i++) {
+  row = [];
+  for (var j = 0; j < cols; j++) {
+    row.push(inputArray[j]);
+  }
+  inputArray.splice(0, cols);
+
+  grid.push(row);
+}
+
+var letters = [];
+for (var i = 0; i < cols; i++) {
+  for (var j = 0; j < rows; j++) {
+    letters.push(grid[j][i]);
+  }
+}
+
+var lettersString = letters.join("");
